ef6cdaf6b9d8acba77227b00e6210b5736d32ae5
Alex Busnita <alexandru.busnita@yahoo.ro>
2017-01-21 20:24:04 +0100

    added resource spawner removed v2/v3 grids and units

---
+++ Assets/Resources/Grid/Scripts/ResourceSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject res_metalsPrefab;
+    [SerializeField]
+    private GameObject res_rocksPrefab;
+    [SerializeField]
+    private GameObject res_crystalsPrefab;
+    [SerializeField]
+    private float launchforce;
+
+    public enum Resources
+    {
+        None,
+        Rock = 5,
+        Metal = 10,
+        Crystal = 20
+    }
+
+    public void SpawnResource(Resources res,Vector3 position, Vector3 direction)
+    {
+        GameObject resource = null;
+        switch (res)
+        {
+            case Resources.Rock:
+                resource = Instantiate(res_rocksPrefab);
+                    break;
+            case Resources.Metal:
+                resource = Instantiate(res_metalsPrefab);
+                    break;
+            case Resources.Crystal:
+                resource = Instantiate(res_crystalsPrefab);
+                break;
+        }
+
+        Rigidbody rb = resource.GetComponent<Rigidbody>();
+        rb.transform.position = position;
+        rb.AddForce(direction * launchforce, ForceMode.Impulse);
+
+    }
+}
