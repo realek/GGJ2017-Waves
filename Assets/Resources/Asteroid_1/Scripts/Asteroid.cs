1078330f170214f086fcd141a7bff972aab57713
Alex Busnita <alexandru.busnita@yahoo.ro>
2017-01-21 13:16:06 +0100

    Asteroid and explosions

--- Assets/Resources/Asteroid_1/Scripts/Asteroid.cs
+++ Assets/Resources/Asteroid_1/Scripts/Asteroid.cs
@@ -6,11 +6,20 @@ public class Asteroid : MonoBehaviour {
 
     [SerializeField]
     private float m_TTL;
+    public GameObject particleSystemExplosionSmall;
+    public GameObject particleSystemExplosionBig;
 
 
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject, m_TTL);
+       var p1 = Instantiate(particleSystemExplosionSmall).GetComponent<ParticleSystem>();
+        p1.transform.position = gameObject.transform.position;
+        p1.Play();
+        var p2 = Instantiate(particleSystemExplosionBig).GetComponent<ParticleSystem>();
+        p2.transform.position = gameObject.transform.position;
+        p2.Play();
+
     }
 
 }
