14ea7d5a3b873570183fc0755dd3fa2a3591bd0f
Alex Busnita <alexandru.busnita@yahoo.ro>
2017-01-22 02:24:18 +0100

    Added asteroid explosions Asteroids get destroyed after impact

---
+++ Assets/Resources/TTLDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TTLDestroy : MonoBehaviour {
+
+    private ParticleSystem m_particleSys;
+	// Use this for initialization
+	void Start () {
+
+        m_particleSys = GetComponent<ParticleSystem>();
+        Destroy(gameObject, m_particleSys.main.duration + 1);
+	}
+}
